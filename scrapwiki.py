15942de91ab09cf510cedf91570815f3a4553bc3
sbeddall <sbeddall@gmail.com>
2020-07-19 16:51:59 -0700

    resolve issue with the ToC links not working. added a serve option if you want to explore locally

--- scrapwiki.py
+++ scrapwiki.py
@@ -1,10 +1,21 @@
 import argparse
 import subprocess
 import os
+import json
 
 root_dir = os.path.abspath(os.path.join(os.path.abspath(__file__), ".."))
 build_ = os.path.join(root_dir, "_internal", "build.py")
-clean_ = os.path.join(root_dir, "_internal", "clean.py")
+target = os.path.join(root_dir, "docs")
+internal_dir = os.path.join(root_dir, "_internal")
+clean = os.path.join(internal_dir, "clean.py")
+config_file = os.path.join(internal_dir, "config.json")
+
+if os.path.exists(config_file):
+    with open(config_file, "r", encoding="utf-8") as f:
+        content = f.read()
+        CONFIG = json.loads(content)
+else:
+    CONFIG = {}
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -17,8 +28,15 @@ if __name__ == "__main__":
 
     if args.target == "build":
         if args.clean:
-            subprocess.check_call(["python", clean_])
+            subprocess.check_call(["python", clean])
 
         subprocess.check_call(["python", build_])
+    elif args.target == "serve":
+        # poller implementation
+        subprocess.check_call(["python", build_])
+        target_port = CONFIG.get("serve_port", "8000")
+        print(target_port)
+
+        subprocess.check_call(["python", "-m", "http.server", target_port], cwd = target)
     else:
         print("Not a recognized command. Exiting.")
